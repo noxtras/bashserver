cf623d4090bfa03fa3b52818e637fdfaf71a8906
George L. <noxtras@outlook.com>
2014-02-25 00:04:21 +0200

    Update server.sh
    added default file (index.html)
    fixed jpg serving and other binary data that contains NUL
    added ph7 execution! (check the ph7 github page) - simple php interpreter in less then 300k
    better security by logging connection time as well as remote IP

--- server.sh
+++ server.sh
@@ -1,77 +1,91 @@
 #!/bin/bash
 # chmod 755 server.sh
 # run ./server.sh
-#open browser, visit http://localhost:8080/index.html
+#open browser, visit http://localhost:8080
 
 RESP=/tmp/webresp
 [ -p $RESP ] || mkfifo $RESP
 
 while true ; do
 
-( cat $RESP ) | nc -l -p 8080 | (
-REQ=`while read L && [ " " "<" "$L" ] ; do echo "$L" ; done`
-url="${REQ#GET }"
-url="${url% HTTP/*}"
-echo "[`date '+%Y-%m-%d %H:%M:%S'`] $REQ" | head -1 >> server.log 2>&1 | echo "[`date '+%Y-%m-%d %H:%M:%S'`] $url"
+( cat $RESP ) | nc -lv 8080 2>con.ip | (
+        REQ=`while read L && [ " " "<" "$L" ] ; do echo "$L" ; done`
+        url="${REQ#GET }"
+        url="${url% HTTP/*}"
+        conip=$(tail -1  con.ip | cut -d'[' -f 2 | cut -d']' -f 1)
+        echo "$conip [`date '+%Y-%m-%d %H:%M:%S'`] $REQ" | head -1 >> server.log 2>&1 | echo "$conip [`date '+%Y-%m-%d %H:%M:%S'`] $url"
 
-#split get into filename, arguments
-url=(${url//\?/ })
-cwd=$(pwd)
-filename="$cwd/www${url[0]}"
+        #split get into filename, arguments
+        url=(${url//\?/ })
+        cwd=$(pwd)
+        file="${url[0]}"
+        if [ "$file" == "/" ]; then
+           file="index.html"
+        fi
 
-#if there is a file
-if [ -f "$filename" ]; then
-#if html, set conten type html, otherwise read from file(needed for Firefox)
-if [[ "${url[0]}" == *.html ]] || [[ "${url[0]}" == *.htm ]] || [[ "${url[0]}" == *.php ]] || [[ "${url[0]}" == *.py ]] || [[ "${url[0]}" == *.cb ]]
-	then
-	  ctype="text/html"
-	else
-	  ctype=$(file --mime-type $filename)
-	  ctype=${ctype#* }
-fi
+        filename="$cwd/www$file"
 
-#list arguments - to pass on to pyton or C
-if [ "${#url[@]}" -gt 1 ]; then
-	arg=${url[1]}
-cont=""
-getarg=(${url[1]//\&/ })
-for x in ${getarg[@]}
-do
-    cont="$cont $x"
-done
+        #if there is a file
+        if [ -f "$filename" ]; then
+        #if html, set conten type html, otherwise read file mime
+           if [[ "$file" == *.html ]] || [[ "$file" == *.htm ]] || [[ "$file" == *.php ]] || [[ "$file" == *.py ]] || [[ "$file" == *.cb ]] || [[ "$file" == *.ph7 ]]
+           then
+                ctype="text/html"
+           else
+             cat "$filename" >$RESP
+             continue
+           #      ctype=$(file --mime-type $filename)
+           #      echo "$ctype - $filename"
+           #      ctype=${ctype#* }
+           fi
 
-fi
-#end argument list
+           #list arguments - to pass on to pyton or C
+           if [ "${#url[@]}" -gt 1 ]; then
+                arg=${url[1]}
+                cont=""
+                getarg=(${url[1]//\&/ })
+                for x in ${getarg[@]}
+                do
+                  cont="$cont $x"
+                done
 
-#if PHP or Python script or binary, process it, show the result to the user, otherwise, just output the file
-filedata=""
-executefile=${url[0]#*/}
-#execute C, Python and PHP scripts
-if [[ "${url[0]}" == *.cb ]]; then
-	filedata=$(cd www && ./$executefile $cont)
-elif [[ "${url[0]}" == *.php ]]; then
-        filedata=$(php $filename $cont)
-elif [[ "${url[0]}" == *.py ]]; then
-        filedata=$(python $filename $cont)
-else
-	filedata=$(<$filename)
-fi
-# Content-Length: ${#filedata}
-	TRES="HTTP/1.1 200 OK
+           fi
+           #end argument list
+
+           #if ph7, PHP, Python script or binary, process it & show the result to the user, otherwise, just output the file
+           filedata=""
+           executefile=${url[0]#*/}
+           #execute C, Python and PHP scripts
+           if [[ "$file" == *.cb ]]; then
+                filedata=$(cd www && ./$executefile $cont)
+           elif [[ "$file" == *.php ]]; then
+                filedata=$(php $filename $cont)
+           elif [[ "$file" == *.ph7 ]]; then
+                filedata=$(./ph7 $filename $cont)
+           elif [[ "$file" == *.py ]]; then
+                filedata=$(python $filename $cont)
+           else
+                filedata=$(<$filename)
+           fi
+           # Content-Length: ${#filedata}
+           TRES="HTTP/1.1 200 OK
+Date: `date '+%a, %d %b %Y %T %Z'`
 Cache-Control: private
-Server: bash/2.1
+Server: NCbash/2.3
+Accept-Ranges: bytes
 Content-Type: $ctype
-Connection: Close
+Connection: close
 Content-Length: ${#filedata}
+Content-Encoding: binary
 
 $filedata"
-else
-	TRES="HTTP/1.1 404 Not Found
+        else
+           TRES="HTTP/1.1 404 Not Found
 Content-Type: text/html
 
 <h1>404 Not Found</h1>
 The requested resource was not found"
-fi
+        fi
 
 cat >$RESP <<EOF
 $TRES
